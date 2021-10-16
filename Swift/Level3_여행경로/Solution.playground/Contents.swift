6da780c8bb03d780fc33d52ecdfd3853336db986
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-10-16 21:24:38 +0900

    chore:add comments to Level3 여행경로
    2021-10-16

--- Swift/Level3_여행경로/Solution.playground/Contents.swift
+++ Swift/Level3_여행경로/Solution.playground/Contents.swift
@@ -16,7 +16,10 @@ func solution(_ tickets:[[String]]) -> [String] {
         }else{
             dictionary.updateValue([ticket[1]], forKey: ticket[0])
         }
-    }
+    } //tickets 배열을 dictionary로 저장
+    //dictionary로 저장한 이유는 알파벳 순서로 정렬 및 dfs때 쉽게 값에 접근하기 위해서였는데
+    //다른 분들 풀이를 보니 그냥 2차원배열에서 도착지 기준으로 정렬 했으면 됐다.
+    
     
     for ticket in tickets {
         if let value = dictionary[ticket[0]]{
@@ -24,7 +27,7 @@ func solution(_ tickets:[[String]]) -> [String] {
             arr.sort()
             dictionary.updateValue(arr, forKey: ticket[0])
         }
-    }
+    }// 정렬 부분
     
     func dfs(_ start:String,_ res:inout [String],_ n:Int,_ dic:[String:[String]]) {
         var dic = dic
@@ -35,7 +38,7 @@ func solution(_ tickets:[[String]]) -> [String] {
         }
         guard let ticket = dic[start] else {return }
         
-        for point in ticket where result.isEmpty
+        for point in ticket where result.isEmpty //만약 이미 결과가 나왔으면 dfs를 수행하지 않는다.
         {
             res.append(point)
             var tic = ticket
