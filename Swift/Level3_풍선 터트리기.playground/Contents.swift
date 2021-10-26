975d2b7d2fdd90d079414a48073444eb1aa4dd18
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-10-26 17:39:53 +0900

    modify Level3_풍선터트리기
    2021-10-26

--- Swift/Level3_풍선 터트리기.playground/Contents.swift
+++ Swift/Level3_풍선 터트리기.playground/Contents.swift
@@ -5,8 +5,8 @@ func solution(_ a:[Int]) -> Int {
     var right = a.count - 1
     var left_min = a[left]
     var right_min = a[right]
-    var answer = 0
-    while left < right{
+    var answer = 2
+    while abs(left-right) != 1{
         if a[left]>a[right]
         {
             left += 1
@@ -26,7 +26,7 @@ func solution(_ a:[Int]) -> Int {
         }
     }
     
-    return answer+1
+    return answer
 }
 
 solution([-16,27,65,-2,58,-92,-71,-68,-61,-33])
