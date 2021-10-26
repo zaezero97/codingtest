f9e480f9f398216dc2fdb949b5c9d1122a28abc8
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-10-26 11:09:37 +0900

    solve Level2_피로도 in Programmers
    2021-10-26

---
+++ Swift/Level2_피로도.playground/Contents.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
+    
+    var visited = [Bool](repeating: false, count: dungeons.count)
+    var max = -1
+    
+    func dfs(n:Int,k:Int){
+        if n == dungeons.count
+        {
+            max = n
+            return
+        }
+        if dungeons.enumerated().filter({ (index,element) in
+            visited[index] == false && k >= element[0]
+        }).count == 0
+        {
+            if max < n
+            {
+                max = n
+            }
+            return
+        }
+        for i in dungeons.indices {
+            if !visited[i] , k >= dungeons[i][0]
+            {
+                visited[i] = true
+                dfs(n: n+1, k: k-dungeons[i][1])
+                visited[i] = false
+            }
+        }
+        
+    }
+    dfs(n: 0,k: k)
+    return -1
+}
+
+solution(80, [[80,20],[50,40],[30,10]])
