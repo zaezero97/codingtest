9592e3f2111ea32d67a5c04af7b65a08fb62d173
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-11-03 09:37:50 +0900

    solve Leve3_이숭우선순위큐 on Programmers
    2021 - 11 -3

---
+++ Swift/Level3_이중우선순위큐.playground/Contents.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+
+func solution(_ operations:[String]) -> [Int] {
+   var split_str = [String]()
+    var arr = [Int]()
+    for str in operations{
+        split_str = str.split(separator:" ").map { String($0)}
+        if split_str[0] == "I"{
+            arr.append(Int(split_str[1])!)
+        }else if !arr.isEmpty
+        {
+            if split_str[1] == "1"{
+                arr.sort(by: >)
+                arr.removeFirst()
+            }else{
+                arr.sort(by: >)
+                arr.removeLast()
+            }
+        }
+    }
+    arr.sort(by: >)
+    if arr.isEmpty{
+        return [0,0]
+    }else{
+        return [arr.first!,arr.last!]
+    }
+}
+
+solution(["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"])
+
