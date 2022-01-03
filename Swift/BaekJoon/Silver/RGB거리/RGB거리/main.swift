b450a61572dcfb34b5eccef576750dcb35e1257d
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2022-01-03 13:44:07 +0900

    Solve Level_Silver RGB거리 on BeakJoon
    2021 - 1 - 3

---
+++ Swift/BaekJoon/Silver/RGB거리/RGB거리/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  RGB거리
+//
+//  Created by 재영신 on 2022/01/03.
+//
+
+// dp 
+import Foundation
+
+let N = Int(readLine()!)!
+var cost = [[Int]](repeating: [Int](repeating: 0, count: 3), count: N)
+
+for i in 0 ..< N {
+    cost[i] = readLine()!.components(separatedBy: " ").compactMap{Int($0)}
+}
+
+for i in 1 ..< N {
+    cost[i][0] += min(cost[i-1][1],cost[i-1][2])
+    cost[i][1] += min(cost[i-1][0],cost[i-1][2])
+    cost[i][2] += min(cost[i-1][0],cost[i-1][1])
+}
+
+print(min(min(cost[N-1][0],cost[N-1][1]),cost[N-1][2]))
