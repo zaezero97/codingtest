f369cb600abe14286866656a55731befcb285dbc
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2022-01-04 10:29:55 +0900

    48ms -> 20ms 로 수정
    2022 - 1 - 4

--- Swift/BaekJoon/Silver/1로 만들기/1로 만들기/main.swift
+++ Swift/BaekJoon/Silver/1로 만들기/1로 만들기/main.swift
@@ -8,18 +8,24 @@
 import Foundation
 
 var N = Int(readLine()!)!
-var dp = [Int](repeating: 0, count: N + 2)
 
-dp[1] = 0
-dp[2] = 1
-for i in 2 ..< N + 1 {
-    dp[i] = dp[i-1] + 1
-    if i % 3 == 0 {
-        dp[i] = min(dp[i],dp[i/3] + 1)
+
+main()
+func main() {
+    var dp = [Int](repeating: 0, count: N + 2)
+
+    dp[1] = 0
+    dp[2] = 1
+    for i in 2 ..< N + 1 {
+        dp[i] = dp[i-1] + 1
+        if i % 3 == 0 {
+            dp[i] = min(dp[i],dp[i/3] + 1)
+        }
+        if i % 2 == 0 {
+            dp[i] = min(dp[i], dp[i/2] + 1)
+        }
     }
-    if i % 2 == 0 {
-        dp[i] = min(dp[i], dp[i/2] + 1)
-    }
+    print(dp[N])
 }
-print(dp[N])
+
 
